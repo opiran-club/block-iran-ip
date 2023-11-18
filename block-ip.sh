b17a222c1bbfcbd9d511c4cbe95d5d004060f1a9
Opiran club <130220895+opiran-club@users.noreply.github.com>
2023-11-18 17:06:38 +0330

    Update block-ip.sh

--- block-ip.sh
+++ block-ip.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
-clear 
 
-# Color variables for dark mode
-RED=$(tput setaf 1)       # Dark red
-YELLOW=$(tput setaf 3)    # Dark yellow
-CYAN=$(tput setaf 6)      # Dark cyan
-GREEN=$(tput setaf 2)     # Dark green
-BLUE=$(tput setaf 4)      # Dark blue
-MAGENTA=$(tput setaf 5)   # Magenta
-WHITE=$(tput setaf 7)     # White
+CYAN="\e[96m"
+GREEN="\e[92m"
+YELLOW="\e[93m"
+RED="\e[91m"
+BLUE="\e[94m "
+MAGENTA="\e[95m"
+NC="\e[0m"
 BOLD=$(tput bold)
-RESET=$(tput sgr0)
+
+logo=$(cat << "EOF"
+    ______    _______   __      _______        __      _____  ___  
+   /    " \  |   __ "\ |" \    /"      \      /""\    (\"   \|"  \ 
+  // ____  \ (. |__) :)||  |  |:        |    /    \   |.\\   \    |
+ /  /    ) :)|:  ____/ |:  |  |_____/   )   /' /\  \  |: \.   \\  |
+(: (____/ // (|  /     |.  |   //      /   //  __'  \ |.  \    \. |
+ \        / /|__/ \    /\  |\ |:  __   \  /   /  \\  \|    \    \ |
+  \"_____/ (_______)  (__\_|_)|__|  \___)(___/    \___)\___|\____\)
+EOF
+)
+
+logo() {
+echo -e "\033[1;34m$logo\033[0m"
+}
 
 # Function to block IPs from a specific country
 block_country_ips() {
@@ -19,11 +31,26 @@ block_country_ips() {
   curl -sSL "https://www.ipdeny.com/ipblocks/data/countries/$country_code.zone" | awk '{print "sudo ufw deny out from any to " $1}' | bash
 }
 
-# Function to print the formatted question and response table
+flush() {
+echo -e "\e[33mUninstalling and flushing UFW rules\e[0m"
+  sudo ufw reset
+  sudo ufw disable
+
+  echo -e "\e[32mUFW rules have been uninstalled and flushed, and also ufw is disabled. \e[0m"
+}
+
 print_table() {
-  echo -e "\e[32m─────────────────────────────────────────────────────────────\e[0m"
-  echo -e "\e[37m  ㅤ      ─────────── OPIRAN PANEL ───────────  \e[0m"
-  echo -e "\e[31m─────────────────────────────────────────────────────────────\e[0m"
+    tg_title="https://t.me/OPIranCluB"
+    yt_title="youtube.com/@opiran-inistitute"
+    clear
+    logo
+    echo -e "\e[93m╔═══════════════════════════════════════════════╗\e[0m"  
+    echo -e "\e[93m║            \e[94mBlock IP                           \e[93m║\e[0m"   
+    echo -e "\e[93m╠═══════════════════════════════════════════════╣\e[0m"
+    echo ""
+    echo -e "${BLUE}   ${tg_title}   ${NC}"
+    echo -e "${BLUE}   ${yt_title}   ${NC}"
+    echo ""
 }
 
 # Install required packages
@@ -44,7 +71,7 @@ print_table
 # Ask the user which country IPs to block
 echo -e "\e[31m!!\e[0m\e[32m Its the common script to block outgoing traffic base on"
 echo -e "country with UFW so after that allow your required ports\e[0m"
-echo -e "\e[31m─────────────────────────────────────────────────────────────\e[0m"
+printf "\e[93m+-----------------------------------------------+\e[0m\n" 
 
 echo -e "\e[33mWhich country IPs do you want to block?\e[0m"
 echo ""
@@ -52,7 +79,9 @@ echo -e "\e[31m1)\e[0m \e[36mIran\e[0m"
 echo -e "\e[31m2)\e[0m \e[36mChina\e[0m"
 echo -e "\e[31m3)\e[0m \e[36mRussia\e[0m"
 echo ""
-echo -e "\e[31m─────────────────────────────────────────────────────────────\e[0m"
+echo -e "\e[31m4)\e[0m \e[36mUninstall and flush rules\e[0m"
+echo ""
+printf "\e[93m+-----------------------------------------------+\e[0m\n" 
 
 read -p "Enter the number of your choice (1/2/3): " choice
 
@@ -67,6 +96,9 @@ case "$choice" in
   3)
     block_country_ips "ru"
     ;;
+  4)
+    flush
+    ;;
   *)
     echo "\e[31mInvalid choice. Exiting...\e[0m"
     exit 1
@@ -77,8 +109,9 @@ esac
 print_table
 
 # Ask the user whether to enable the firewall or not
-read -p "\e[33mDo you want to enable the firewall\e[0m \e[31m(without enabling ufw the function won't work correctly)\e[0m? \e[33m(yes/no):\e[0m " enable_firewall
-echo -e "\e[31m─────────────────────────────────────────────────────────────\e[0m"
+echo -ne "\e[33mDo you want to enable the firewall\e[0m \e[31m(without enabling ufw the function won't work correctly)\e[0m? \e[33m(yes/no):\e[0m "
+read enable_firewall
+printf "\e[93m+-----------------------------------------------+\e[0m\n" 
 
 if [[ "$enable_firewall" =~ ^(Y|y|YES|yes)$ ]]; then
   ufw enable
